a6700e2dec9e617f91d41a2f564c706c7e6859d2
jacktolosapalou <36288716+jacktolosapalou@users.noreply.github.com>
2018-03-15 21:00:44 +0800

    Add files via upload

--- Test.js
+++ Test.js
@@ -1,5 +1,4 @@
 
-
 function loadUrl(url, func) {
 
 var xhr = new XMLHttpRequest()
@@ -14,10 +13,12 @@ xhr.addEventListener( 'load', function()  {
     }
 });
     
-xhr.open('GET', "http://jservice.io/api/categories?count=5&offset=10")
+xhr.open('GET', url)
 xhr.send()
   
 }
+
+
 loadUrl("http://jservice.io/api/categories?count=5&offset=10", function(xhr)
 {
 var table = document.getElementById('output');
@@ -29,9 +30,11 @@ var table = document.getElementById('output');
     var fourth = table.insertRow(3);
     var fifth = table.insertRow(4);
     var sixth = table.insertRow(5);
+    
     first.setAttribute("id", "first");
     var data = xhr.response;
     var jsonCat = JSON.parse(data);
+
    console.log(jsonCat);
 
     for(i= 0; i<5; i++)
@@ -42,20 +45,30 @@ var table = document.getElementById('output');
     }
 
 
-       loadUrl("http://jservice.io/api/category?id=" + jsonCat[0].id, function(xhr)
+    loadUrl("http://jservice.io/api/category?id=" + jsonCat[0].id, function(xh)
     {
-        var quest1=xhr.response;
+        var quest1=xh.response;
         var jsonQuest1 = JSON.parse(quest1);
-        console.log(jsonQuest1);
+        console.log(jsonQuest1.clues[0].value);
+        var table = document.getElementById('output');
+        var second = table.insertRow(1);
 
         for(i=0; i<5; i++)
         {
             var td2 =second.insertCell(i);
-            td2.innerHTML=jsonQuest1[i].value;
+            td2.innerHTML=jsonQuest1.clues[0].value;
+            var td3 = third.insertCell(i);
+            td3.innerHTML=jsonQuest1.clues[1].value;
+            var td4 = fourth.insertCell(i);
+            td4.innerHTML=jsonQuest1.clues[2].value;
+            var td5 = fifth.insertCell(i);
+            td5.innerHTML=jsonQuest1.clues[3].value;
+            var td6 = sixth.insertCell(i);
+            td6.innerHTML=jsonQuest1.clues[4].value;
+
+
+
         }
     });
 
-
 });
-
-
